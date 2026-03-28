f1f5143390d5a4cc0bad0211f6f925dcbaefe1c7
Prakyath Nandigam <prakyathnandigam9999@gmail.com>
2026-03-28 10:00:00 +0530

    Add CIVIQ logo and branding component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+export default function Logo() {
+  return (
+    <div className="logo-block">
+      <div className="logo-row">
+        <div className="logo-icon">🗳</div>
+        <div className="logo-text">
+          CIV<span>IQ</span>
+        </div>
+      </div>
+      <div className="logo-tagline">Democracy, Demystified.</div>
+    </div>
+  );
+}
