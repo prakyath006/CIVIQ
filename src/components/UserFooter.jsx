b623d941733992c38e37e9e59b37f1967e36fc82
Prakyath Nandigam <prakyathnandigam9999@gmail.com>
2026-04-01 09:30:00 +0530

    Add user footer to sidebar

---
+++ src/components/UserFooter.jsx
@@ -0,0 +1,13 @@
+export default function UserFooter() {
+  return (
+    <div className="user-footer">
+      <div className="user-footer-row">
+        <div className="user-avatar">U</div>
+        <div className="user-info">
+          <div className="user-name">Citizen</div>
+          <div className="user-status">First-time Voter</div>
+        </div>
+      </div>
+    </div>
+  );
+}
